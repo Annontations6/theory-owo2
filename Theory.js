aedad1bddd0543ab9b6fafa125fb5e79a44f6f40
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-03 16:29:23 -0300

    nice

--- Theory.js
+++ Theory.js
@@ -17,16 +17,34 @@ var a = BigNumber.ZERO;
 
 var init = () => {
     currency = theory.createCurrency();
+
+    // Ki
+    {
+        let getDesc = (level) => "K_i=" + getKI(level).toString(3);
+        Ki = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(2))));
+        Ki.getDescription = (_) => Utils.getMath(getDesc(Ki.level));
+        Ki.getInfo = (amount) => Utils.getMathTo(getDesc(Ki.level), getDesc(Ki.level + amount));
+    }
 }
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
-    a += BigNumber.ONE
-    currency.value += dt * a;
+    a += BigNumber.ONE * getKI(Ki.level)
+    currency.value += dt * a * getKI(Ki.level);
     theory.invalidateSecondaryEquation();
+}
+
+var getPrimaryEquation = () => {
+    let result = "\\dot{\\rho} = a";
+
+    result += " \\times K_i";
+
+    return result;
 }
 
 var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho^{0.9}, a = " + a.toString(3);
 var getTau = () => currency.value.pow(0.9);
+
+var getKI = (level) => BigNumber.from(1 + 0.036 * level)
 
 init();
