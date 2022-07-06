eafbf55623c4c64347cea5dbeaaf88546bf4678e
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-05 23:46:55 -0300

    can!

--- Sannie.js
+++ Sannie.js
@@ -15,6 +15,11 @@ var currency;
 var templateCost = [BigNumber.from("1e1500"), BigNumber.from("1e1500"), BigNumber.from("ee4")]
 var templateMath = ["t_1", "t_2", "t_3"]
 var templates = new Array(3)
+var varsCost = [10, 300, 40000, 1e7]
+var currencyInsert = [new ExponentialCost(varsCost[0], Math.log2(3)), new ExponentialCost(varsCost[1], Math.log2(3)), new ExponentialCost(varsCost[2], Math.log2(3)), new ExponentialCost(varsCost[3], Math.log2(3))]
+var varsMath = ["w", "x", "y", "z"]
+var getLevelVars = (level) => [Utils.getStepwisePowerSum(level, 2.5, 9, 0), Utils.getStepwisePowerSum(level, 3, 10, 0), BigNumber.from((50) * level), Utils.getStepwisePowerSum(level, 3, 10, 0)]
+var vars = new Array(4)
 
 var init = () => {
     currency = theory.createCurrency();
@@ -22,10 +27,18 @@ var init = () => {
     ///////////////////
     // Regular Upgrades
 
+    // All 4 Variables
+
+    for (let i = 0; i < 4; i++) {
+        vars[i] = theory.createUpgrade(i, currency, currencyInsert[i])
+        vars[i].getDescription = (_) => "My Formula Is " + Utils.getMath("x") + " | You Gained " + getLevelVars[i] + " Rho Per Second of Variable!"
+        vars[i].getInfo = (amount) => "Coming soon..."
+    }
+
     // All 3 Templates
 
     for (let i = 0; i < 3; i++) {
-        templates[i] = theory.createUpgrade(i, currency, new ExponentialCost(templateCost[i], Math.log2(1)));
+        templates[i] = theory.createUpgrade(i + 4, currency, new ExponentialCost(templateCost[i], Math.log2(1)));
         templates[i].getDescription = (_) => "Template " + (i + 1);
         templates[i].getInfo = (amount) => "Given Template Now!";
         templates[i].maxLevel = 1;
