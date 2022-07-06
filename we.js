af4c2ec13598e865be17c1118b2c097881457764
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-06 10:18:28 -0300

    wow

--- we.js
+++ we.js
@@ -27,7 +27,7 @@ var init = () => {
     b = theory.createCurrency("b", "b");
     g = theory.createCurrency("g", "g");
 
-    // ? Functions
+    // 3 Functions
 
     /**
      * Returneds Value of A Collect i think :)
@@ -39,20 +39,37 @@ var init = () => {
 
     /**
      * Addiontial a Big Number Spent
-     * @param {string} a Add Plus When Everything
-     * @param {string} b Plus Show a + b (5 + 3 = 8).
+     * @param {number} a Add Plus When Everything
+     * @param {number} b Plus Show a + b (5 + 3 = 8).
      * @returns {string} add this know when string 
      */
     function Addiontoial(a, b) {
         return BigNumber.from(a + b)
     }
 
+    /**
+     * Two of Power of BigNumber
+     * @param {number} a Returns Two of pow
+     * @param {string} name Returns Name not show
+     * @param {boolean} isUnlocked Returns unlock not visible
+     * @return {string} i string hi
+     */
+    function TwoPow(a, nme, isUnlocked) {
+        if (isunlocked) {
+            return BigNumber.TWO.pow(a)
+        } else {
+            return "..."
+        }
+    }
+
     // Regular Upgrades
 
     // a
     {
-        a = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
+        a = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(40, Math.log2(2.25))));
         a.getDescription = (_) => "You gained " + a_click + " a per click!";
-        a.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
+        a.getInfo = (amount) => "Gained " + a_click + " a click";
     }
 }
+
+init();
