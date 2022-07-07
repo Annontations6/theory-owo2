4cf91272c27ac332485b4ab61a651504f373892c
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-07 19:41:20 -0300

    WHEN

--- THEORY+/Theory.js
+++ THEORY+/Theory.js
@@ -62,6 +62,6 @@ var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.16
 var getTau = () => currency.value;
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
-var getJ =  Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getJ = () =>  Utils.getStepwisePowerSum(level, 2, 10, 0);
 
 init();
